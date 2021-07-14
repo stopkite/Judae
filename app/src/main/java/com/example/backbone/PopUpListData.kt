dc67d66dd3d94d20c9e3a03006f5e997f3aa92f7
stopkite <62979643+stopkite@users.noreply.github.com>
2021-07-15 01:17:29 +0900

    저장 팝업 변수/클래스 명 변경

---
+++ app/src/main/java/com/example/backbone/PopUpListData.kt
@@ -0,0 +1,4 @@
+package com.example.backbone
+
+// 카테고리에 들어갈 데이터 내용
+data class PopUpListData(val categoryName:String)
--- app/src/main/java/com/example/backbone/SavingActivity.kt
+++ app/src/main/java/com/example/backbone/SavingActivity.kt
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.backbone.databinding.ActivitySavingBinding
 
-// 카테고리에 들어갈 내용 data 클래스 생성 (28 번째 줄에 쓰일 예정)
-data class CategoryList(val categoryName:String)
+
 
 class SavingActivity : AppCompatActivity() {
     // 리스트뷰에 붙일 adapter 변수 생성
-    private lateinit var categoryAdapter: SelectCatAdapter
+    private lateinit var popUpAdapter: PopUpAdapter
 
     // 현재 액티비티에서 보여줄 xml 화면 생성
     private lateinit var binding: ActivitySavingBinding
@@ -25,20 +24,20 @@ class SavingActivity : AppCompatActivity() {
         val cateList = binding.cateList
 
         // CategoryList 클래스를 담는 배열 생성
-        val categoryList = ArrayList<CategoryList>()
+        val categoryList = ArrayList<PopUpListData>()
 
         // 카테고리에 들어갈 목록들 삽입(임의로 넣은 데이터) : 기능 연결할 때 이 부분 다뤄줘야함...
-        categoryList.add(CategoryList("우정"))
-        categoryList.add(CategoryList("사랑"))
-        categoryList.add(CategoryList("진로"))
-        categoryList.add(CategoryList("가족"))
-        categoryList.add(CategoryList("감정"))
+        categoryList.add(PopUpListData("우정"))
+        categoryList.add(PopUpListData("사랑"))
+        categoryList.add(PopUpListData("진로"))
+        categoryList.add(PopUpListData("가족"))
+        categoryList.add(PopUpListData("감정"))
 
         // adapter 초기화
-        categoryAdapter = SelectCatAdapter(this,categoryList)
+        popUpAdapter = PopUpAdapter(this,categoryList)
 
         // 리스트뷰에 방금 생성한 adapter를 붙여서 화면에 연결해준다.
-        cateList.adapter = categoryAdapter
+        cateList.adapter = popUpAdapter
 
     }
 }
