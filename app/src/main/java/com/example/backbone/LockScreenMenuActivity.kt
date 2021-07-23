f9fb40f232bb224adfd505e707f761b9b3911093
a-young <dkdudab@hanmail.net>
2021-07-23 23:12:50 +0900

    앱 암호 설정 스위치 화면 띄우기

--- app/src/main/java/com/example/backbone/LockScreenMenuActivity.kt
+++ app/src/main/java/com/example/backbone/LockScreenMenuActivity.kt
@@ -1,5 +1,6 @@
 package com.example.backbone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.backbone.databinding.ActivityLockScreenMenuBinding
@@ -10,6 +11,22 @@ class LockScreenMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLockScreenMenuBinding.inflate(layoutInflater)
+
+        // 앱 암호 설정 스위치 이동시 창 뜨게 하는 버튼 리스너
+        binding.switchBtn.setOnCheckedChangeListener { CompoundButton, onSwitch ->
+            if (onSwitch){
+                // 스위치 활성화
+                val lockScreenSetIntent = Intent(this@LockScreenMenuActivity, LockScreenSetActivity::class.java)
+                startActivity(lockScreenSetIntent)
+            }
+
+            else {
+                //스위치 비활성화
+                val lockScreenIntent = Intent(this@LockScreenMenuActivity, LockedScreenActivity::class.java)
+                startActivity(lockScreenIntent)
+            }
+
+        }
 
         setContentView(binding.root)
     }
