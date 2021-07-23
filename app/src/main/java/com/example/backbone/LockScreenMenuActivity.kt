44333a6a49bd64a50f469fb78ad21816580688a6
a-young <dkdudab@hanmail.net>
2021-07-24 03:29:51 +0900

    앱 암호 설정 화면 Exist 완료. 앱 암호 설정 switch 구현. 앱 암호 변경 리스너 연결. 등등.. 개많음..ㅎㅎ

--- app/src/main/java/com/example/backbone/LockScreenMenuActivity.kt
+++ app/src/main/java/com/example/backbone/LockScreenMenuActivity.kt
@@ -1,33 +1,73 @@
 package com.example.backbone
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
 import com.example.backbone.databinding.ActivityLockScreenMenuBinding
+
 
 class LockScreenMenuActivity : AppCompatActivity() {
     private lateinit var binding:ActivityLockScreenMenuBinding
 
+    //DBHelper와 이어주도록 클래스 선언
+    var db:DBHelper= DBHelper(this)
+
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityLockScreenMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try{
+            //User 테이블이 있는지 SQL문으로 돌려보기
+            db.PWisExist()
+            binding.switchBtn.isChecked = true
+            binding.changePWTxt.isEnabled = false
+        }catch (e: Exception){
+            //존재 하지 않으면 익셉션 발생하므로 그냥 flag 변수 false로 설정해주기.
+            //만약 사용자 User 테이블이 존재하지 않는다면 바로 홈 화면으로 넘어가기
+            binding.switchBtn.isChecked = false
+            binding.changePWTxt.isEnabled = true
+            binding.changePWTxt.setTextColor(Color.parseColor("#808080"))
+        }
+
+        //뒤로가기 버튼 클릭 리스너
+        binding.backBtn.setOnClickListener {
+            // 홈 화면으로 이동
+            finish()
+        }
 
         // 앱 암호 설정 스위치 이동시 창 뜨게 하는 버튼 리스너
         binding.switchBtn.setOnCheckedChangeListener { CompoundButton, onSwitch ->
             if (onSwitch){
-                // 스위치 활성화
-                val lockScreenSetIntent = Intent(this@LockScreenMenuActivity, LockScreenSetActivity::class.java)
+                // 스위치 활성화시 암호 설정 화면으로 이동
+                val lockScreenSetIntent = Intent(this@LockScreenMenuActivity, LockedScreenSetActivity::class.java)
                 startActivity(lockScreenSetIntent)
             }
 
             else {
-                //스위치 비활성화
-                val lockScreenIntent = Intent(this@LockScreenMenuActivity, LockedScreenActivity::class.java)
+                //스위치 비활성화시 암호 확인 화면으로 이동
+                val lockScreenIntent = Intent(this@LockScreenMenuActivity, LockedScreenOnceActivity::class.java)
                 startActivity(lockScreenIntent)
             }
 
         }
 
+        // 암호 변경 창 뜨게 하는 버튼 리스너
+        binding.changePWTxt.setOnClickListener {
+            // 앱 암호 설정 화면으로 이동
+            val lockScreenSetIntent = Intent(this@LockScreenMenuActivity, LockedScreenOnceActivity::class.java)
+            startActivity(lockScreenSetIntent)
+        }
+
         setContentView(binding.root)
     }
+
+
+
 }
--- app/src/main/java/com/example/backbone/LockedScreenOnceActivity.kt
+++ app/src/main/java/com/example/backbone/LockedScreenOnceActivity.kt
@@ -7,12 +7,13 @@ import android.view.View
 import android.widget.Toast
 import com.example.backbone.databinding.ActivityLockedScreenBinding
 import com.example.backbone.databinding.LockScreenSetBinding
+import com.example.backbone.databinding.LockScreenSetOnceBinding
 import java.lang.Exception
 
-//암호 설정 액티비티
-class LockedScreenSetActivity : AppCompatActivity(), View.OnClickListener {
+//암호 확인 액티비티
+class LockedScreenOnceActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var binding:LockScreenSetBinding
+    private lateinit var binding:LockScreenSetOnceBinding
 
     // 비밀번호 4자리를 저장할 배열 생성
     private var PWList= mutableListOf<String>()
@@ -41,7 +42,7 @@ class LockedScreenSetActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = LockScreenSetBinding.inflate(layoutInflater)
+        binding = LockScreenSetOnceBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
@@ -59,21 +60,14 @@ class LockedScreenSetActivity : AppCompatActivity(), View.OnClickListener {
         binding.keyDel.setOnClickListener(this)
         binding.keyReset.setOnClickListener(this)
 
-        //체크 버튼 클릭 리스너
-        binding.pwSetBtn.setOnClickListener {
-            // 암호 설정 화면으로 이동
-            val lockSetIntent = Intent(this@LockedScreenSetActivity, LockScreenMenuActivity::class.java)
-            startActivity(lockSetIntent)
-            finish()
-        }
-
         //뒤로가기 버튼 클릭 리스너
         binding.backBtn.setOnClickListener {
             // 암호 설정 화면으로 이동
-            //val lockSetIntent = Intent(this@LockedScreenSetActivity, LockScreenMenuActivity::class.java)
+            //val lockSetIntent = Intent(this@LockedScreenOnceActivity, LockScreenMenuActivity::class.java)
             //startActivity(lockSetIntent)
             finish()
         }
+
     }
 
     // 해당 버튼을 클릭했을 때 PWList 배열에 해당 숫자를 입력받는다
@@ -178,6 +172,25 @@ class LockedScreenSetActivity : AppCompatActivity(), View.OnClickListener {
             4 -> {  PW4 = PWList[3]
                 binding.PW4.setColorFilter(resources.getColor(R.color.purple_200))
                 passCode = PW1 + PW2 + PW3 + PW4 // 4자리 비밀번호 입력받기 완료!
+
+                // 여기서 passCode가 기존에 등록해 놓은 것과 일치하면 홈화면으로 통과시키기
+                // 기존에 등록해 놓은 비밀번호 내용 불러오기
+                UserPassWord = db.getUserPassWord()
+                if(passCode == UserPassWord)
+                {
+                    //두 내용이 일치하면 암호 설정 화면으로 넘어가기
+                    val lockSetIntent = Intent(this@LockedScreenOnceActivity, LockedScreenSetActivity::class.java)
+                    startActivity(lockSetIntent)
+                    finish()
+                }
+                else{
+                    //비밀번호 일치 하지 않으면 -> 일치하지 않는다는 토스트 메시지 띄우고, 입력되었던 내용 다 지우기
+                    Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
+                    //입력되었던 내용 다 지우고
+                    PWList.removeAll(PWList)
+                    //해당 내용 UI에 반영하기 위해 콜백 함수로 이용.
+                    passNumber(PWList)
+                }
 
             }
         }
