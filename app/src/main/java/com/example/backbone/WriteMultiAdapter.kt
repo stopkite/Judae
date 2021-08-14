d0e794af5deab342df3ce034e9c7629e2ce1e43f
a-young <dkdudab@hanmail.net>
2021-08-14 22:23:29 +0900

    글쓰기에서 사진 변경 수정

--- app/src/main/java/com/example/backbone/WriteMultiAdapter.kt
+++ app/src/main/java/com/example/backbone/WriteMultiAdapter.kt
@@ -513,8 +513,7 @@ class WriteMultiAdapter(writingActivity: WritingActivity, context:Context): Recy
 
                             // 변경 버튼을 클릭했을 때
                             if (which == 0) {
-                                activity.openGalleryForImage()
-                                removeItems(position)
+                                EditGalleryImage(WriteList)
                             }
                             // 삭제 버튼을 클릭했을 때
                             else if (which == 1) {
